fc0b9cb771437fa924c1517495ead53cb1eaeb58
Seviko Attalarik <attalarik.handoko@gmail.com>
2025-06-10 07:28:46 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   preview: {
     port: 5173,
     strictPort: true,
